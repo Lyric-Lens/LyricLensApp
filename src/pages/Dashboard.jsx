bf6824bdb1fa42569f83d41a685ce67541c1f2cb
Drajad-Kusuma-Adi <drajadkusumaadi@gmail.com>
2024-03-30 18:51:19 +0700

    search and play music done (no music control yet)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { api } from "../utils/API";
+import Music from "../components/Music";
 
 export default function Dashboard() {
   // Determine page: Home, Explore, Collection, Profile
@@ -11,7 +12,7 @@ export default function Dashboard() {
     }
 
     // Redirect on invalid page
-    const routes = ['home', 'explore', 'collection', 'profile'];
+    const routes = ['home', 'explore', 'collection', 'profile', 'search'];
     if (!routes.includes(page)) {
       window.location.search = '?page=home';
     }
@@ -33,23 +34,121 @@ export default function Dashboard() {
     })
   }, [photo])
 
+  // Search
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const searchExecuted = useRef(false);
+  useEffect(() => {
+    if (search !== '') {
+      window.location.search = `?page=search&search=${search}`
+    }
+  }, [search])
+  useEffect(() => {
+    if (searchExecuted.current) return;
+    searchExecuted.current = true;
+
+    let searchParams = new URLSearchParams(window.location.search).get('search');
+    if (searchParams !== null) {
+      api.post('/searchMusic', {
+        search: searchParams
+      }, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        }
+      })
+      .then((res) => {
+        setSearchResults(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err.response.data.message);
+      })
+    }
+  }, [])
+  function searchMusic(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    setSearch(formData.get('search'));
+  }
+
   return (
     <>
-      <div className="bg-gradient-to-b from-[#111] to-[#111B2B] w-screen h-screen">
-        <img src="Logo.svg" alt="LyricLens logo as the background" className="opacity-25 w-[280px] h-[280px] absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ left: '50%', position: 'absolute' }} />
+      <div className="bg-[url('Background.png')] bg-cover bg-no-repeat bg-center w-screen h-screen">
+
+        {page === 'search' && (
+          <>
+            {/* Header - Search bar and settings */}
+            <div className="flex justify-around items-center p-4">
+
+              {/* Search bar */}
+              <label className="input input-bordered flex items-center gap-2 rounded-full">
+                <form onSubmit={searchMusic}>
+                  <input type="text" className="grow" placeholder="Search" name="search" id="search" defaultValue={new URLSearchParams(window.location.search).get('search')} />
+                  <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                  </button>
+                </form>
+              </label>
+
+              {/* Settings */}
+              <img src="Settings.svg" alt="Settings button" className="btn btn-ghost w-[56px] h-[56px]" onClick={()=>document.getElementById('settingsModal').showModal()} />
+
+            </div>
+
+              {/* Search results */}
+              <div className="h-[75vh] overflow-y-scroll">
+                {searchResults.map((val, i) => {
+                  return (
+                    <Music val={val} key={i} />
+                  )
+                })}
+              </div>
+
+            {/* Settings dialog */}
+            <dialog id="settingsModal" className="modal">
+              <div className="modal-box">
+                <div className="flex justify-between items-center">
+                  <h3 className="font-bold text-lg">Settings</h3>
+                  <form method="dialog">
+                    <button className="btn btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f9f9f9" className="bi bi-x" viewBox="0 0 16 16">
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                    </svg>
+                    </button>
+                  </form>
+                </div>
+                <div className="py-4">
+                  {/* Logout */}
+                  <p onClick={function() { localStorage.removeItem('email'); localStorage.removeItem('token'); location.reload(); }} className="text-red-400">Log out</p>
+                </div>
+              </div>
+            </dialog>
+          </>
+        )}
+
+        {/* Home page */}
         {page === 'home' && (
         <>
           {/* Header - Search bar and settings */}
           <div className="flex justify-around items-center p-4">
+
             {/* Search bar */}
             <label className="input input-bordered flex items-center gap-2 rounded-full">
-              <input type="text" className="grow" placeholder="Search" />
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+              <form onSubmit={searchMusic}>
+                <input type="text" className="grow" placeholder="Search" name="search" id="search" />
+                <button type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                </button>
+              </form>
             </label>
 
             {/* Settings */}
             <img src="Settings.svg" alt="Settings button" className="btn btn-ghost w-[56px] h-[56px]" onClick={()=>document.getElementById('settingsModal').showModal()} />
+
           </div>
+
+          {/* Settings dialog */}
           <dialog id="settingsModal" className="modal">
             <div className="modal-box">
               <div className="flex justify-between items-center">
@@ -63,22 +162,26 @@ export default function Dashboard() {
                 </form>
               </div>
               <div className="py-4">
+                {/* Logout */}
                 <p onClick={function() { localStorage.removeItem('email'); localStorage.removeItem('token'); location.reload(); }} className="text-red-400">Log out</p>
               </div>
             </div>
           </dialog>
         </>
         )}
+
         {page === 'explore' && (
         <>
         explore
         </>
         )}
+
         {page === 'collection' && (
         <>
         collection
         </>
         )}
+
         {page === 'profile' && (
         <>
         profile
